a0b9dded664c22f13d4550408913406384822ddd
Jean-Philippe Ulpiano <jeanphi@AirdeJePhilippe.fritz.box>
2023-09-24 13:39:34 +0200

    Updated game

--- services/application_service.py
+++ services/application_service.py
@@ -14,8 +14,9 @@ def start():
     screen: Canvas = Canvas('Candy Cat', screen_width, screen_height, Common.START_MUSIC)
 
     create_scene_service: CreateSceneService = CreateSceneService(
-        [Common.GAME_NAME + 'assets/levels/game4',
-         Common.GAME_NAME + 'assets/levels/game2',
+        [Common.GAME_NAME + 'assets/levels/game2',
+         Common.GAME_NAME + 'assets/levels/game3',
+         Common.GAME_NAME + 'assets/levels/game1',
          Common.GAME_NAME + 'assets/levels/game1'],screen)
 
 
